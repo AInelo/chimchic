3322a1f9f37b7ce9e106c3f569fbc35c0203b638
Lionel TOTON <totonlionel@gmail.com>
2024-03-18 11:19:30 +0100

    Evolution du site de campagne

--- public/js/fedapayPayement.js
+++ public/js/fedapayPayement.js
@@ -1,26 +1,4 @@
-const {FedaPay, Transaction} = require('fedapay');
-
-// FedaPay.setApiKey("sk_live_CaZH-pW0S9lqhGldYsaQf0C2");
-
-// FedaPay.setEnvironment('live');
-
-// const transaction = await Transaction.create({
-//     descripition: "Frais de Formation CHIMCHIC",
-//     amount: 2000,
-//     callback_url: 'https://maplateforme.com/callback',
-//     currency: {
-//         iso: 'XOF'
-//     },
-//     customer: {
-//         firstname: "Lionel",
-//         lastname: "TOTON",
-//         email: "totonlionel@gmail.com",
-//         phone_number: {
-//             number: '96769716',
-//             country: 'BJ'
-//         }
-//     }
-// });
+// const {FedaPay, Transaction} = require('fedapay');
 
 async function createTransaction () {
     const {FedaPay, Transaction} = require('fedapay');
