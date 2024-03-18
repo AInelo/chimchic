f2d1fe995a9c953f671aff7595505897d1e77151
Lionel TOTON <totonlionel@gmail.com>
2024-03-18 02:08:56 +0100

    Evolution du site de campagne

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,15 @@ const path = require("path");
 
 module.exports = {
   mode: "development", // ou 'production' selon vos besoins
-  entry: "./fedapayPayement.js",
+  entry: "./public/js/fedapayPayement.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    fallback: {
+      path: require.resolve("path-browserify"),
+      crypto: require.resolve("crypto-browserify"),
+    },
   },
 };
